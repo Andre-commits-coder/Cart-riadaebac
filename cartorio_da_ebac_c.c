2afae5459ff9e7ff93e94225d19aaa2b425dfb79
André Luiz Dos Reis <andreoutbook1982@gmail.com>
2024-12-15 03:19:51 -0300

    Opção de sáida
    Foi adicionado a opção de sáida para o usuário do sistema.

--- cartorio_da_ebac_c.c
+++ cartorio_da_ebac_c.c
@@ -127,6 +127,7 @@ int main()
         printf("\t1 - Registar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar\n");
+        printf("\t4 - sair do sistema\n");
         printf("Opção: "); // Fim do menu
 
         scanf("%d", &opcao); // Armazenando a escolha do usuário
@@ -145,6 +146,11 @@ int main()
 
             case 3:
             deletar();
+            break;
+            
+            case 4:
+            printf ("Obrigado por utilizar o sistema!\n");
+            return 0;
             break;
 
             default:
